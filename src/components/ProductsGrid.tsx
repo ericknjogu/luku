b766495385518ea67fa56e1057095b0ce956fcb5
erick <njoguerick3@gmail.com>
2025-02-20 21:01:00 +0300

    edit

--- src/components/ProductsGrid.tsx
+++ src/components/ProductsGrid.tsx
@@ -1,9 +1,37 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import HomeTabbar from "./HomeTabbar";
+import { productType } from "@/constants";
+import { client } from "@/sanity/lib/client";
 
 export default function ProductsGrid() {
-  const [selectedTab, setSelectedTab] = useState("");
+  const [selectedTab, setSelectedTab] = useState(productType[0]?.title || "");
+
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const query = `*[_type == "product" && variant == $variant] | order(name asc)`;
+
+  const params = { variant: selectedTab.toLocaleLowerCase() };
+
+  console.log(params);
+  console.log(query);
+
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      try {
+        const response = await client.fetch(query, params);
+        console.log(response);
+      } catch (error) {
+        console.log("product fetching error", error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, [selectedTab]);
+
   return (
     <div className="mt-10 flex flex-col items-center">
       <HomeTabbar selectedTab={selectedTab} onTabSelect={setSelectedTab} />
